e659a2995a0125f73beda94855c53ece63c7bb3a
emanuel constancio <emanuel.constancio@claro.com.ar>
2024-07-05 14:43:49 -0300

    fixing content type problem

--- src/utils/postService.ts
+++ src/utils/postService.ts
@@ -1,10 +1,22 @@
 export const postService = (body: any) => {
-    fetch("http://localhost:8080/createCategory", {method: "POST", body})  
-        .then(response => response.json())
-        .then((res)=>{
-            console.log(res)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-}
+  console.log(body, " soy body");
+  fetch("http://localhost:8080/createCategory", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body), // Asegúrate de convertir el cuerpo a JSON
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
