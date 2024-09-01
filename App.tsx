bfd9e79d85994b736b1190bf492b07185ddd8ecb
user <user@EZE1-LMC-B08054.fibertel.com.ar>
2024-09-01 16:31:33 -0300

    adding imports

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import React, { FC, useContext, useState } from "react"
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native';
 import CategoryForm from "./src/components/organisms/CategoryForm";
 import Routes from "./src/routes/Routes";
+import { AuthContext, AuthProvider } from "./src/context/AuthContext";
+import RootNavigator from "./src/routes/Routes";
 
 export default function App() {
 
