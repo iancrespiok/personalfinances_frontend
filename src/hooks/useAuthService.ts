caa27f29c559b889bb54166f32c8e7741e42377b
leandro-natanael-dev <leandro.almiron.dev@gmail.com>
2024-09-09 18:49:36 -0300

    created hook useAuthService

---
+++ src/hooks/useAuthService.ts
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+
+/**
+ * 
+ * @returns 
+ */
+
+export const useAuthService = () => {
+    const { storeToken, setIsAuthenticated } = useContext(AuthContext);
+  
+    const postAuthService = async (body: any, endpoint: string) => {
+      const baseUrl = process.env.EXPO_PUBLIC_URLBASE;
+      const url = baseUrl + endpoint;
+  
+      try {
+        const response = await axios.post(url, body, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const token = response.data.jwt;
+        if (token) {
+          await storeToken(token);
+          setIsAuthenticated(true);
+        }
+  
+        return response.data;
+      } catch (error) {
+        console.error('Error posting to auth service:', error);
+        throw error; // Puedes manejar el error como consideres necesario
+      }
+    };
+  
+    return { postAuthService };
+  };
+  
