090f39c671b20e5b159dc97d843b4583c50579f2
leandro-natanael-dev <leandro.almiron.dev@gmail.com>
2024-09-13 20:35:05 -0300

    create useGetService, deleted getService

---
+++ src/hooks/useGetService.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useToken } from "../hooks/useToken";
+
+const useGetService = (endpoint: string) => {
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    getService();
+  }, [endpoint]);
+
+  const getService = async () => {
+    try {
+      setLoading(true);
+      const { token, error: tokenError } = await useToken();
+      if (tokenError) {
+        setError(true);
+      }
+      const urlBase = process.env.EXPO_PUBLIC_URLBASE;
+      const url = urlBase + endpoint;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`, 
+          "Content-Type": "application/json", 
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const responseData = await response.json();
+      setData(responseData);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error };
+};
+
+export default useGetService;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,47 +1,33 @@
 import React, { FC, useEffect, useState } from 'react'
 import { View, StyleSheet, Switch } from 'react-native'
-import dataTest from '../utils/dataTest.json'
 import TransactionHistory from '../components/organisms/TransactionHistory/TransactionHistory'
 import { Dimensions } from 'react-native'
-import getService from '../utils/getService'
+import useGetService from '../hooks/useGetService'
 
 interface Props {
     navigation: any
 }
 const HomeScreen: FC<Props> = () => {
+
     const { width } = Dimensions.get('window');
     const styles = createStyles(width);
-
-
-    // useEffect(() => {
-    //     getService('moneyFlows')
-    //         .then((data) => console.log("Test", data))
-    
-    //     }, []);
+    const { data, loading, error } = useGetService("moneyflows");
 
     return (
         <View style={styles.container}>
             <TransactionHistory
-                dataRender={dataTest}
+                dataRender={data}
             />
         </View >
     );
 
 }
-
-
 const createStyles = (width: number) => StyleSheet.create({
     container: {
         width: width,
         backgroundColor: 'white',
     },
-    darkMode: {
-        backgroundColor: 'black',
-    }
-
 })
-
-
 
 export default HomeScreen
 
